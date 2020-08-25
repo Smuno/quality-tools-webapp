428cbbd2f5b18b0081be509ef7455994b685b200
Samuel <samuel.munoz@swarm.cl>
2020-08-25 17:51:23 -0400

    init - Se distribuye la tarjeta en 3 partes: Header, Body y Footer para Antecedentes, Resultados y [future use]

---
+++ src/components/ResultCard/ResultCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <b-card no-body>
+        <!--  -->
+        <b-card-header></b-card-header>
+        <b-card-body></b-card-body>
+        <b-card-footer></b-card-footer>
+    </b-card>
+</template>
+
+<script>
+export default {
+    name:"ResultCard",
+    props:{
+        header:Object,
+        body:Object,
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+
+    },
+
+}
+</script>
