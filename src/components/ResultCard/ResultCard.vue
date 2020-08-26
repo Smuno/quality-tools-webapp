428f300827114753cd40e868d4905d4e9fd3fdf4
Samuel <samuel.munoz@swarm.cl>
2020-08-26 10:38:55 -0400

    definiendo data on ResultCard ¿que es un resultado? 	Props: 		header: 			title 			badges 				text 				variant 		body 			text

--- src/components/ResultCard/ResultCard.vue
+++ src/components/ResultCard/ResultCard.vue
@@ -1,27 +1,52 @@
 <template>
-    <b-card no-body>
-        <!--  -->
-        <b-card-header></b-card-header>
-        <b-card-body></b-card-body>
-        <b-card-footer></b-card-footer>
-    </b-card>
+  <b-card no-body>
+    <!--  -->
+    <b-card-header>
+      <h1>{{ header.title }}</h1>
+      <b-badge
+        v-for="(badge, index) in header.badges"
+        :key="index"
+        :variant="badge.variant"
+      >
+        {{ badge.text }}
+      </b-badge>
+    </b-card-header>
+    <b-card-body>
+      <!-- 
+        texto usuario
+        resultado ¿que es un resultado?
+        -->
+      <p><span v-html="body.text"></span></p>
+    </b-card-body>
+    <b-card-footer> </b-card-footer>
+  </b-card>
 </template>
 
 <script>
 export default {
-    name:"ResultCard",
-    props:{
-        header:Object,
-        body:Object,
+  name: "ResultCard",
+  props: {
+    header: {
+      type: Object,
+      default: {
+        title: "Sin titulo",
+        badges: [
+          { text: "Default", variant: "info" },
+          { text: "No Data", variant: "danger" }
+        ]
+      }
     },
-    data() {
-        return {
-            
-        }
-    },
-    methods:{
-
-    },
-
-}
+    body: {
+      type: Object,
+      default: {
+        text: "Sample text of something here",
+        
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
 </script>
