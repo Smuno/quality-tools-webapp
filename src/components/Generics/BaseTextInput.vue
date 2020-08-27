f4011ca0f2f8cde873f30b6e354b9d7de6b58e1a
Samuel <samuel.munoz@swarm.cl>
2020-08-27 16:13:20 -0400

    add: logic to display menu

--- src/components/Generics/BaseTextInput.vue
+++ src/components/Generics/BaseTextInput.vue
@@ -1,7 +1,7 @@
 <template>
   <b-container>
     <!-- Menu para editar texto -->
-    <b-row no-gutters v-if="isEditable">
+    <b-row no-gutters v-if="isEditable" v-show="showMenu">
       <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
         <b-button-toolbar>
           <b-button-group size="sm">
@@ -10,39 +10,40 @@
               :variant="variantType(isActive.paragraph())"
               @click="commands.paragraph"
             >
-              <b-icon icon="paragraph"></b-icon>
+              <b-icon icon="paragraph" />
             </b-button>
             <b-button
               title="Bold"
               :variant="variantType(isActive.bold())"
               @click="commands.bold"
             >
-              <b-icon icon="type-bold"></b-icon>
+              <b-icon icon="type-bold" />
             </b-button>
             <b-button
               title="Italic"
               :variant="variantType(isActive.italic())"
               @click="commands.italic"
             >
-              <b-icon icon="type-italic"></b-icon>
+              <b-icon icon="type-italic" />
             </b-button>
 
             <b-dropdown text="Header">
               <b-dropdown-item @click="commands.heading({ level: 1 })">
-                <b-icon icon="type-h1"></b-icon>
+                <b-icon icon="type-h1" />
               </b-dropdown-item>
               <b-dropdown-item @click="commands.heading({ level: 2 })">
-                <b-icon icon="type-h2"></b-icon>
+                <b-icon icon="type-h2" />
               </b-dropdown-item>
               <b-dropdown-item @click="commands.heading({ level: 3 })">
-                <b-icon icon="type-h3"></b-icon>
+                <b-icon icon="type-h3" />
               </b-dropdown-item>
             </b-dropdown>
+            
           </b-button-group>
         </b-button-toolbar>
       </editor-menu-bar>
     </b-row>
-<!-- Espacio de edicion de texto -->
+    <!-- Espacio de edicion de texto -->
     <b-row no-gutters>
       <b-col>
         <EditorContent class="editorContent" :editor="editor" />
@@ -53,6 +54,9 @@
 
 <script>
 //! evento: "new-text"
+// TODO Revisar si data es responsiva sin onUpdate
+//* editable or no editable -> no mostrar menu y no editar
+//? ¿está en una carta de resultado? -> mostrar menu on focus y editar
 //import Icon from 'Components/Icon'
 import { Editor, EditorContent, EditorMenuBar } from "tiptap";
 import {
@@ -80,26 +84,32 @@ export default {
     EditorContent,
     EditorMenuBar
   },
-  props:{
-    isEditable:{
-      type:Boolean,
-      default:true,
+  props: {
+    isEditable: {
+      type: Boolean,
+      default: true
     },
-    contentForEditor:{
-      type:String,
-      default:"<h1>Hola!,</h1><p>Ejemplo de texto, un poco largo para ver como queda y ver aun si extiendo por sobre</p>",
+    contentForEditor: {
+      type: String,
+      default:
+        "<h1>Hola!,</h1><p>Ejemplo de texto, un poco largo para ver como queda y ver aun si extiendo por sobre</p>"
+    },
+    onCard: {
+      type: Boolean,
+      default: false
     }
   },
   data() {
     return {
       keepInBounds: true,
       editor: null,
-      textJSON: null,
-      isOnFocus:false,
+      textHTML: null,
+      isOnFocus: false
     };
   },
   methods: {
     variantType: function(isActive) {
+      // Para cambiar estilo de botones menu
       if (isActive) {
         return "dark";
       } else {
@@ -107,17 +117,34 @@ export default {
       }
     }
   },
-  computed: {},
+  computed: {
+    showMenu: function() {
+      /* Saber si mostrar de manera permanente 
+       mostrar permanente
+       o toglear en foco del editor*/
+      if (this.onCard && this.isEditable) {
+        if (this.isOnFocus) {
+          return true;
+        } else {
+          return false;
+        }
+      } else if (this.isEditable) {
+        return true;
+      }
+    }
+  },
   watch: {
-    textJSON: function(newVal) {
+    textHTML: function(newVal) {
       this.$emit("new-text", newVal);
     }
   },
   mounted() {
+    const timeAfterTyping = 1000;
     let timeOutTyping = null;
+
     this.editor = new Editor({
-      editable:this.isEditable,
-      content:this.contentForEditor,
+      editable: this.isEditable,
+      content: this.contentForEditor,
       extensions: [
         new Heading({ levels: [1, 2, 3] }),
         new ListItem(),
@@ -127,9 +154,15 @@ export default {
       onUpdate: value => {
         clearTimeout(this.timeoutTyping);
         this.timeoutTyping = setTimeout(() => {
-          this.textJSON = value.getHTML();
+          this.textHTML = value.getHTML();
           console.log(value.getHTML());
-        }, 1000);
+        }, timeAfterTyping);
+      },
+      onFocus: focusData => {
+        this.isOnFocus = true;
+      },
+      onBlur: blurData => {
+        this.isOnFocus = false;
       }
     });
   },
